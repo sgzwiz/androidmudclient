5f2097c42c2370f483d2d4a73c1ce51abb2da370
TimothySwartz <TimothySwartz@51c86cde-8012-11de-8688-af8986fd7f98>
2009-08-05 00:44:27 +0000

    Base Android eclipse project

---
+++ src/com/andmudclient/andmudclient.java
@@ -0,0 +1,13 @@
+package com.andmudclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class andmudclient extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
